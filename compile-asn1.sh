04d6fdc423db0b8d1dc37a3d43baa4ee03ab2bb7
Alberto Leiva Popper <ydahhrk@gmail.com>
2026-03-19 12:02:03 -0300

    Fix autogen seds in MacOS
    1. The argument string of sed -i argument is mandatory in MacOS.
    2. It also seems the sed expressions were too long, so I redesigned
       them.

--- compile-asn1.sh
+++ compile-asn1.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ -z "$ASN1C" ]; then
+	ASN1C="asn1c"
+fi
+
 die() {
 	echo "$1"
 	exit 1
@@ -10,32 +14,18 @@ OUTPUT_DIR=libasn1fort
 rm -fr $OUTPUT_DIR || die "Could not delete directory $OUTPUT_DIR."
 mkdir -p $OUTPUT_DIR || die "Could not create directory '$OUTPUT_DIR'."
 
-asn1c -Werror -fcompound-names -fwide-types -D $OUTPUT_DIR \
+"$ASN1C" -Werror -fcompound-names -fwide-types -D $OUTPUT_DIR \
 		-no-gen-PER -no-gen-example \
 		asn1/*.asn1 || die "Compilation failed."
 
-# Build a regular expression consisting of the $OUTPUT_DIR files, joined by
-# pipes:
-# 1. ls: Get a list of file names in $OUTPUT_DIR, one per line
-#          AlgorithmIdentifier.c
-#          AlgorithmIdentifier.h
-#          ANY.c
-#          ANY.h
-#          etc
-# 2. paste: join the file names using a pipe character. ("or" character in
-#    regexps.)
-#          AlgorithmIdentifier.c|AlgorithmIdentifier.h|ANY.c|ANY.h|etc
-# 3. sed: Escape the periods. (so they won't mean "anything".)
-#          AlgorithmIdentifier\.c|AlgorithmIdentifier\.h|ANY\.c|ANY\.h|etc
-LS=$(ls -1 $OUTPUT_DIR | paste -sd "|" - | sed -E "s/\./\\\./g")
+# Replace '#include <file.h>' with '#include <$OUTPUT_DIR/file.h>' everywhere.
+sed -Ei "" 's!#include [<"](.*)\.h[">]!#include <libasn1fort/\1.h>!' \
+	$OUTPUT_DIR/*.c $OUTPUT_DIR/*.h
 
-# Replace '#include <file.h>' with '#include <$OUTPUT_DIR/file.h> mostly
-# everywhere.
-sed -Ei 's!#include <('"$LS"')>!#include <'"$OUTPUT_DIR"'/\1>!' $OUTPUT_DIR/*.c $OUTPUT_DIR/*.h
-sed -Ei 's!#include "('"$LS"')"!#include <'"$OUTPUT_DIR"'/\1>!' $OUTPUT_DIR/*.c $OUTPUT_DIR/*.h
-# If you're wondering why we bothered with LS instead of something like "(.+\.h)",
-# it's because not all #includes need the treatment. For example, some files
-# include <errno.h>, which obviously does not belong to $OUTPUT_DIR.
+# Restore the system includes.
+SYSTEM_INCLUDES="assert|errno|float|inttypes|limits|malloc|netinet/in|stdarg|stddef|stdint|stdio|stdlib|string|sys/types|time|types/vxTypes|windows"
+sed -Ei "" "s!#include <libasn1fort/($SYSTEM_INCLUDES)\.h>!#include <\1.h>!" \
+	$OUTPUT_DIR/*.c $OUTPUT_DIR/*.h
 
 # Change the default name of the library
-sed -i 's/libasncodec/libasn1fort/' libasn1fort/Makefile.am.libasncodec
+sed -i '' 's/libasncodec/libasn1fort/' libasn1fort/Makefile.am.libasncodec
