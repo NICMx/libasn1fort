7031ff56511d82d975f50490b704a5091efcdbc4
Alberto Leiva Popper <ydahhrk@gmail.com>
2026-03-27 12:18:32 -0600

    Fix autogen seds in Linux

--- compile-asn1.sh
+++ compile-asn1.sh
@@ -19,13 +19,13 @@ mkdir -p $OUTPUT_DIR || die "Could not create directory '$OUTPUT_DIR'."
 		asn1/*.asn1 || die "Compilation failed."
 
 # Replace '#include <file.h>' with '#include <$OUTPUT_DIR/file.h>' everywhere.
-sed -Ei "" 's!#include [<"](.*)\.h[">]!#include <libasn1fort/\1.h>!' \
+sed -Ei"" 's!#include [<"](.*)\.h[">]!#include <libasn1fort/\1.h>!' \
 	$OUTPUT_DIR/*.c $OUTPUT_DIR/*.h
 
 # Restore the system includes.
 SYSTEM_INCLUDES="assert|errno|float|inttypes|limits|malloc|netinet/in|stdarg|stddef|stdint|stdio|stdlib|string|sys/types|time|types/vxTypes|windows"
-sed -Ei "" "s!#include <libasn1fort/($SYSTEM_INCLUDES)\.h>!#include <\1.h>!" \
+sed -Ei"" "s!#include <libasn1fort/($SYSTEM_INCLUDES)\.h>!#include <\1.h>!" \
 	$OUTPUT_DIR/*.c $OUTPUT_DIR/*.h
 
 # Change the default name of the library
-sed -i '' 's/libasncodec/libasn1fort/' libasn1fort/Makefile.am.libasncodec
+sed -i'' 's/libasncodec/libasn1fort/' libasn1fort/Makefile.am.libasncodec
